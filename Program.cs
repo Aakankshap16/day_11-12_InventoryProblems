32b072cc91680547012caa195013b9952d13930a
Aakankshap16 <aakankshap971@gmail.com>
2023-03-26 00:00:33 +0530

    add program file for UC2 with price calculated

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@
                 Console.WriteLine(data.typeOfRice[i].weight);
                 Console.WriteLine(data.typeOfRice[i].price);
                 int val = data.typeOfRice[i].weight * data.typeOfRice[i].price;
-               
+                Console.WriteLine("The price of {0} for {1} kg is {2}Rs", data.typeOfRice[i].name, data.typeOfRice[i].weight,val);
 
                 Console.Write(" ------------------------------");
             }
@@ -35,7 +35,7 @@
                 Console.WriteLine(data.typeOfWheat[i].weight);
                 Console.WriteLine(data.typeOfWheat[i].price);
                 int val = data.typeOfWheat[i].weight * data.typeOfWheat[i].price;
-                
+                Console.WriteLine("The price of {0} for {1} kg is {2}Rs", data.typeOfWheat[i].name, data.typeOfWheat[i].weight, val);
                 Console.WriteLine(" ------------------------------");
             }
 
@@ -51,7 +51,7 @@
                 Console.WriteLine(data.typeOfPulses[i].weight);
                 Console.WriteLine(data.typeOfPulses[i].price);
                 int val = data.typeOfPulses[i].weight * data.typeOfPulses[i].price;
-                
+                Console.WriteLine("The price of {0} for {1} kg is {2}Rs", data.typeOfPulses[i].name, data.typeOfPulses[i].weight, val);
                 Console.WriteLine(" ------------------------------");
             }
 

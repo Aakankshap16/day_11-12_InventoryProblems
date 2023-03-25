1d5019af2c3acec9369c0a88daf89ca8ef6355ea
Aakankshap16 <aakankshap971@gmail.com>
2023-03-25 23:53:03 +0530

    add program file for UC1

--- Program.cs
+++ Program.cs
@@ -2,10 +2,59 @@
 {
     class Program
     {
-        public static void Main(string[] args) 
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Inventory Managment.");
-        }
+            string path = "C:\\Users\\ASHISH PANDEY\\source\\repos\\InventoryMangement\\InventoryMangement\\Inventory.json";
+            ReadInventory obj = new ReadInventory();
+            InventoryDetails data = obj.Read(path);
 
+            Console.WriteLine("Type of Rice");
+            Console.WriteLine("***********************");
+            Console.WriteLine("***********************");
+            for (int i = 0; i < data.typeOfRice.Count; i++)
+            {
+                Console.WriteLine(" ------------------------------");
+                Console.WriteLine(data.typeOfRice[i].name);
+                Console.WriteLine(data.typeOfRice[i].weight);
+                Console.WriteLine(data.typeOfRice[i].price);
+                int val = data.typeOfRice[i].weight * data.typeOfRice[i].price;
+               
+
+                Console.Write(" ------------------------------");
+            }
+
+            Console.WriteLine("***********************");
+            Console.WriteLine("***********************");
+            Console.WriteLine("Type of Wheat");
+            Console.WriteLine("***********************");
+            Console.WriteLine("***********************");
+            for (int i = 0; i < data.typeOfWheat.Count; i++)
+            {
+                Console.WriteLine(" ------------------------------");
+                Console.WriteLine(data.typeOfWheat[i].name);
+                Console.WriteLine(data.typeOfWheat[i].weight);
+                Console.WriteLine(data.typeOfWheat[i].price);
+                int val = data.typeOfWheat[i].weight * data.typeOfWheat[i].price;
+                
+                Console.WriteLine(" ------------------------------");
+            }
+
+            Console.WriteLine("***********************");
+            Console.WriteLine("***********************");
+            Console.WriteLine("Type of Pulses");
+            Console.WriteLine("***********************");
+            Console.WriteLine("***********************");
+            for (int i = 0; i < data.typeOfPulses.Count; i++)
+            {
+                Console.WriteLine(" ------------------------------");
+                Console.WriteLine(data.typeOfPulses[i].name);
+                Console.WriteLine(data.typeOfPulses[i].weight);
+                Console.WriteLine(data.typeOfPulses[i].price);
+                int val = data.typeOfPulses[i].weight * data.typeOfPulses[i].price;
+                
+                Console.WriteLine(" ------------------------------");
+            }
+
+        }
     }
 }

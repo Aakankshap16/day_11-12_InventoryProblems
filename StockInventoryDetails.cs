60d736578a26994b83eef97b48b2978bbbc0f493
Aakankshap16 <aakankshap971@gmail.com>
2023-03-26 11:14:09 +0530

    create list of stock or initilize the data of inventory

---
+++ StockInventoryDetails.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace InventoryMangement
+{
+    public class StockInventoryDetails
+    {
+        public List<Stock1> stock1;
+        public List<Stock2> stock2;
+        public List<Stock3> stock3;
+    }
+
+    public class Stock1
+    {
+        public string StockNames;
+        public int NumberofShare;
+        public int SharePrice;
+    }
+
+    public class Stock2
+    {
+        public string StockNames;
+        public int NumberofShare;
+        public int SharePrice;
+    }
+
+    public class Stock3
+    {
+        public string StockNames;
+        public int NumberofShare;
+        public int SharePrice;
+    }
+}

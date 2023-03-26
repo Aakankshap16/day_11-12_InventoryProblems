64aa91ce8f72b818bb651902080947af6e8f9a12
Aakankshap16 <aakankshap971@gmail.com>
2023-03-26 11:23:15 +0530

    read the path of json

---
+++ ReadInventory.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMangement
+{
+    public class ReadInventory
+    {
+        public StockInventoryDetails Read(string path)
+        {
+            try
+            {
+                using (StreamReader file = new StreamReader(path))
+                {
+                    string json = file.ReadToEnd();      ///read file and store in json///
+
+                    return JsonConvert.DeserializeObject<StockInventoryDetails>(json); ////convert stream of bytes in object///
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+    }
+}
